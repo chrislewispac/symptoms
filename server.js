2c381ac0168aca35a9eda78a8c16da024ce3f8eb
chrislewispac@gmail.com <mail1201>
2015-10-27 10:36:17 -0400

    committed by parse-cli at: 2015-10-27 10:36:17.075427105 -0400 EDT

--- server.js
+++ server.js
@@ -1,37 +1,56 @@
 // Require Node Modules
-var http = require('http'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    //Parse = require('parse/node'),
-    //ParseCloud = require('parse-cloud-express');
-
-    var app = express();
-
-// Import your cloud code (which configures the routes)
-//require('./cloud/main.js');
-// Mount the webhooks app to a specific path (must match what is used in scripts/register-webhooks.js)
-app.use('/webhooks', ParseCloud.app);
-
-// Host static files from public/
-app.use(express.static(__dirname + '/public'));
-
-app.set('views', __dirname + '/public');
-app.set('view engine', 'jade');
-
-app.get('/', function (request, response) {
-    response.render('public/test');
-});
-
-// Catch all unknown routes.
-//app.all('/', function (request, response) {
-//    response.status(404).send('We gettin there.');
+//var http = require('http'),
+//    express = require('express'),
+//    bodyParser = require('body-parser'),
+//    //Parse = require('parse/node'),
+//    //ParseCloud = require('parse-cloud-express');
+//
+//    var app = express();
+//
+//// Import your cloud code (which configures the routes)
+////require('./cloud/main.js');
+//// Mount the webhooks app to a specific path (must match what is used in scripts/register-webhooks.js)
+//app.use('/webhooks', ParseCloud.app);
+//
+//// Host static files from public/
+//app.use(express.static(__dirname + '/public'));
+//
+//app.set('views', __dirname + '/public');
+//app.set('view engine', 'jade');
+//
+//app.get('/', function (request, response) {
+//    response.render('public/test');
+//});
+//
+//// Catch all unknown routes.
+////app.all('/', function (request, response) {
+////    response.status(404).send('We gettin there.');
+////});
+//
+///*
+// * Launch the HTTP server
+// */
+//var port = process.env.PORT || 5000;
+//var server = http.createServer(app);
+//server.listen(port, function () {
+//    console.log('Cloud Code Webhooks server running on port ' + port + '.');
 //});
 
-/*
- * Launch the HTTP server
- */
-var port = process.env.PORT || 5000;
-var server = http.createServer(app);
-server.listen(port, function () {
-    console.log('Cloud Code Webhooks server running on port ' + port + '.');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (request, response) {
+    response.render('pages/index');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
