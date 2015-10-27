fd710ba9da920df4833c315ac166deeffd34b2a2
chrislewispac@gmail.com <mail1201>
2015-10-27 15:44:34 -0400

    committed by parse-cli at: 2015-10-27 15:44:34.3668499 -0400 EDT

--- server.js
+++ server.js
@@ -57,7 +57,7 @@
          headers: ParseHeaders.headers,
          json: true
      }, function (err, res, body) {
-         console.log(body);
+         //console.log(body);
          console.log(body.results);
          //         var newArray = [];
          //
