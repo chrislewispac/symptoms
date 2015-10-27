5571cd37330033b169a6dbc26d9da3213ed06767
chrislewispac@gmail.com <mail1201>
2015-10-27 10:54:18 -0400

    committed by parse-cli at: 2015-10-27 10:54:18.169683246 -0400 EDT

--- server.js
+++ server.js
@@ -19,15 +19,15 @@
  app.set('views', __dirname + '/views');
  app.set('view engine', 'jade');
 
- app.get('/'function (req, res) {
+ app.get('/', function (req, res) {
      res.render('test');
 
  });
 
  //Catch all unknown routes.
-// app.all('/', function (request, response) {
-//     response.status(404).send('We gettin there.');
-// });
+ // app.all('/', function (request, response) {
+ //     response.status(404).send('We gettin there.');
+ // });
 
  /*
   * Launch the HTTP server
