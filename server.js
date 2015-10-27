fbd24478a9c0563de0acdf2efaa4fec2654f021e
chrislewispac@gmail.com <mail1201>
2015-10-27 10:43:04 -0400

    committed by parse-cli at: 2015-10-27 10:43:04.856510579 -0400 EDT

--- server.js
+++ server.js
@@ -1,121 +1,35 @@
-// Require Node Modules
-//var http = require('http'),
-//    express = require('express'),
-//    bodyParser = require('body-parser'),
-//    //Parse = require('parse/node'),
-//    //ParseCloud = require('parse-cloud-express');
-//
-//    var app = express();
-//
-//// Import your cloud code (which configures the routes)
-////require('./cloud/main.js');
-//// Mount the webhooks app to a specific path (must match what is used in scripts/register-webhooks.js)
-//app.use('/webhooks', ParseCloud.app);
-//
-//// Host static files from public/
-//app.use(express.static(__dirname + '/public'));
-//
-//app.set('views', __dirname + '/public');
-//app.set('view engine', 'jade');
-//
-//app.get('/', function (request, response) {
-//    response.render('public/test');
-//});
-//
-//// Catch all unknown routes.
-////app.all('/', function (request, response) {
-////    response.status(404).send('We gettin there.');
-////});
-//
-///*
-// * Launch the HTTP server
-// */
-//var port = process.env.PORT || 5000;
-//var server = http.createServer(app);
-//server.listen(port, function () {
-//    console.log('Cloud Code Webhooks server running on port ' + port + '.');
-//});
+ //Require Node Modules
+ var http = require('http'),
+     express = require('express'),
+     bodyParser = require('body-parser'),
+     Parse = require('parse/node'),
+     ParseCloud = require('parse-cloud-express');
 
-// Parse Cloud Code Webhooks example for Express JS on Heroku
+ var app = express();
 
-// Require Node Modules
-var http = require('http'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    Parse = require('parse').Parse;
+ // Import your cloud code (which configures the routes)
+ require('./cloud/main.js');
 
-// Make sure to set your Webhook key via heroku config set
-var webhookKey = process.env.WEBHOOK_KEY;
+ // Mount the webhooks app to a specific path (must match what is used in scripts/register-webhooks.js)
+ app.use('/webhooks', ParseCloud.app);
 
-// Express middleware to enforce security using the Webhook Key
-function validateWebhookRequest(req, res, next) {
-    if (req.get('X-Parse-Webhook-Key') !== webhookKey) return errorResponse(res, 'Unauthorized Request.');
-    next();
-}
+ // Host static files from public/
+ app.use(express.static(__dirname + '/public'));
 
-// Parse middleware to inflate a beforeSave object to a Parse.Object
-function inflateParseObject(req, res, next) {
-    var object = req.body.object;
-    var className = object.className;
-    var parseObject = new Parse.Object(className);
-    parseObject._finishFetch(object);
-    req.body.object = parseObject;
-    next();
-}
+ app.set('views', __dirname + '/public');
+ app.set('view engine', 'jade');
 
-function successResponse(res, data) {
-    data = data || true;
-    res.status(200).send({
-        "success": data
-    });
-}
 
-function errorResponse(res, message) {
-    message = message || true;
-    res.status(200).send({
-        "error": message
-    });
-}
+ Catch all unknown routes.
+ app.all('/', function (request, response) {
+     response.status(404).send('We gettin there.');
+ });
 
-var app = express();
-var jsonParser = bodyParser.json();
-
-app.use(validateWebhookRequest);
-app.use(jsonParser);
-
-/*
- * Define routes here
- */
-
-app.post('/success', inflateParseObject, function (req, res) {
-    var requestData = req.body;
-    requestData.object.set('extra', 'fizzbuzz');
-    successResponse(res, requestData.object);
-});
-
-app.post('/error', function (req, res) {
-    errorResponse(res, "No thanks.");
-});
-
-app.post('/hello', function (req, res) {
-    successResponse(res, "Hello!");
-});
-
-app.post('/addNumbers', function (req, res) {
-    var params = req.body.params;
-    successResponse(res, params.a + params.b);
-});
-
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.end('{"error":"Request Failed."}');
-});
-
-/*
- * Launch the HTTPS server
- */
-var port = process.env.PORT || 443;
-var server = http.createServer(app);
-server.listen(port, function () {
-    console.log('Cloud Code Webhooks server running on port ' + port + '.');
-});
+ /*
+  * Launch the HTTP server
+  */
+ var port = process.env.PORT || 5000;
+ var server = http.createServer(app);
+ server.listen(port, function () {
+     console.log('Cloud Code Webhooks server running on port ' + port + '.');
+ });
