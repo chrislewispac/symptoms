323e058e95854f5611ac59e7082cbec4549d78d4
chrislewispac@gmail.com <mail1201>
2015-10-28 13:59:21 -0400

    committed by parse-cli at: 2015-10-28 13:59:20.97550158 -0400 EDT

--- server.js
+++ server.js
@@ -69,7 +69,8 @@
          app.set('parseData', parseData);
          app.set('SNPs', SNPs);
          app.set('scope', SNPs + '%20genomes%20names');
-         app.expose(app.settings);
+         app.expose(app.settings, 'settings');
+         console.log(app.settings);
      };
 
      request.get({
