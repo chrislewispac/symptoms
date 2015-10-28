b2201695b5a59e9be47802cca6b7dfc1c101696c
chrislewispac@gmail.com <mail1201>
2015-10-28 17:17:56 -0400

    committed by parse-cli at: 2015-10-28 17:17:56.357589285 -0400 EDT

--- public/js/results.js
+++ public/js/results.js
@@ -1,32 +1,7 @@
 (function resultsModule($) {
 
-    //SET GLOBAL CONFIGURATIONS IN IIFE
-    var appConfig = function () {
-
-        //INITIALIZE PARSE
-        Parse.initialize(
-            "OXrgauxvwBSl8F0PZ5GolOS9a097JFk3gPpHckqg",
-            "uzKaAcJiOWszwU0h4ei8eXMcyMgyNw2c7AjH7F4T"
-        );
-
-        var _andMe = {
-            client_id: 'e361afa06e36942902b04cb8c05e540d',
-            client_secret: '4bc3ca7d6ccb84406fa0cad2dcd8c2fc'
-        }
-
-        // Object literal
-        return {
-            andMe: _andMe,
-        }
-    }();
-
     //RUNS WHEN DOM READY
     $(function uiComponents() {
-
-        console.log('running');
-        console.log(snps);
-        console.log(genotypes);
-        console.log(parseData);
 
         //INITIATE ACCORDION MODULE
         $('.ui.accordion').accordion();
